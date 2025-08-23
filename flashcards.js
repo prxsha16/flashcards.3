4a2c82fdb6071dcf4e8df5ce74d44c794314b355
prxsha16 <prishaa.jain2@gmail.com>
2025-08-23 17:10:29 +0100

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,3 +1,11 @@
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+
 // Load flashcards from localStorage
 let flashcards = JSON.parse(localStorage.getItem("flashcards")) || [];
 let current = 0;
